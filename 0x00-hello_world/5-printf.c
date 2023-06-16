4aedf8202d9d4f3ccbcf7e8a79432ecf4428eab6
Mostafa1Jamal1 <mostafa.jamal.mjm@gmail.com>
2023-06-15 18:04:10 -0700

    modified style: 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  *
  * printf function to print a string
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}  
+}
