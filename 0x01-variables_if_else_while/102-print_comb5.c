ff09ce01eefd3d594a6ad78bd4653eba4551d9f0
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-06-18 17:39:23 +0300

    Did it the hard way🤓

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,26 +17,34 @@ int main(void)
 
 	for (n1 = 48; n1 <= 57; n1++)
 	{
-		for (n2 = 48; n2 <= 56; n2++)
+		for (n2 = 48; n2 <= 57; n2++)
 		{
-			for (n3 = n1; n3 <= 57; n3++)
+			for (n3 = 48; n3 <= 57; n3++)
 			{
-				for (n4 = n2; n4 <= 57; n4++)
+				for (n4 = 48; n4 <= 57; n4++)
 				{
+					if ((n1 >= n3) && (n2 >= n4))
+					{
+						n3 = n1;
+						n4 = n2 + 1;
+						if (n4 == 58)
+							break;
+					}
 					putchar(n1);
 					putchar(n2);
 					putchar(' ');
 					putchar(n3);
 					putchar(n4);
-					if ((n1 + n2 + n3 + n4) != 227)
+					if ((n1 == 57) && (n2 == 56) && (n3 == 57) && (n4 == 57))
 					{
-						putchar(',');
-						putchar(' ');
+						putchar('\n');
+						return (0);
 					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
