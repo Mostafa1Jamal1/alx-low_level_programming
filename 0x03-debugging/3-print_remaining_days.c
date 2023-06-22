e8fb7af6e395f662d3b12de59f5f190f792382d7
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-06-22 23:19:58 +0300

    modified:   3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,35 +16,37 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (year % 100 == 0)
 		{
-			if (year % 400 == 0)
+			if (year % 400 == 0)	/* this is a leap year */
 			{
-				/* this is a leap year */
 				if (month >= 2 && day >= 60)
-				{
 					day++;
-				}
 
 				printf("Day of the year: %d\n", day);
 				printf("Remaining days: %d\n", 366 - day);
 				return;
 
+			}	/* this is not a leap year */
+			if (month == 2 && day == 60)
+				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			else
+			{
+				printf("Day of the year: %d\n", day);
+				printf("Remaining days: %d\n", 365 - day);
+				return;
 			}
-			/* this is not a leap year */
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-			return;
-		}
-		/* this is a leap year */
+		}	/* this is a leap year */
 		if (month >= 2 && day >= 60)
-		{
 			day++;
-		}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 		return;
+	}	/* this is not a leap year */
+	if (month == 2 && day == 60)
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 	}
-	/* this is not a leap year */
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
 }
