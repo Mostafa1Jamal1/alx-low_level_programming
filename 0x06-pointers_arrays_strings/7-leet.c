0f674abec051e3c68efb1d19a2f26857406f393b
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-02 07:34:13 +0300

    modified:   7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
- * only use one if in your code
+ * only use one (IF) in your code
  * only use two loops in your code
  * not allowed to use switch
  * not allowed to use any ternary operation
  *
- * @str: the string to encode
+ * @s: the string to encode
  *
  * Return: the string pointer after encoding
 */
