d2643bed9707e6dc5d18139cc54cdf56b8620b65
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-02 12:02:25 +0300

    modified:   104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,12 +15,13 @@
 int main(void)
 {
 	long max = 1000000000000000; /** 15 zeros*/
-	long prev1, curr1, sum1, sum2 = 0;
+	long prev1, curr1, sum1, sum2;
 	long prev2 = 1;
 	long curr2 = 2;
 	int fib_num = 98;
 	int i;
 
+	prev1 = curr1 = sum1 = sum2 = 0;
 	printf("%ld, ", prev2);
 	printf("%ld, ", curr2);
 	for (i = 2; i < fib_num; i++)
