cc7fc3f2afc75ef420c9b3e137fb4d7855a9bd9a
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-02 10:58:41 +0300

    modified:   104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,27 +12,34 @@
  * Return: 0 (Success)
  */
 
-int main(void)
+int main()
 {
-	long double x = 1;
-	long double y = 2;
-	long double t;
-	int num_fib = 98;
+	long max = 1000000000000000; /** 15 zeros*/
+	long prev1 = 0;
+	long prev2 = 1;
+	long curr1 = 0;
+	long curr2 = 2;
+	long sum1 = 0;
+	long sum2 = 0;
+	int fib_num = 70;
 	int i;
-
-	for (i = y; i <= num_fib; i++)
+	printf("%d, ", prev2);
+	printf("%d, ", curr2);
+	for (i = 2; i < fib_num; i++)
 	{
-		if (x == 1)
-		{
-			printf("%.0Lf, ", x);
-		}
-		printf("%.0Lf", y);
-		if (i != num_fib)
+		sum2 = curr2 + prev2;
+		sum1 = (sum2 / max) + curr1 + prev1;
+		sum2 = sum2 % max;
+		prev1 = curr1;
+		prev2 = curr2;
+		curr1 = sum1;
+		curr2 = sum2;
+		if (curr1 != 0)
+			printf("%d",curr1);
+		printf("%d",curr2);
+		if (i < (fib_num - 1))
 			printf(", ");
-		t = y;
-		y = x + y;
-		x = t;
 	}
 	printf("\n");
-	return (0);
+	return 0;
 }
