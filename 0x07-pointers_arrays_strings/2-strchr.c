60f2d7eea2c318d3fb36bd5f297bea8273599e72
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-03 17:33:33 +0300

    modified:   2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-	char *ptr;
-
-	for (i = 0; *(s + i) != 0; i++)
-		if (*(s + i) == c)
-		{
-			ptr = (s + i);
-			return (ptr);
-		}
+	while (*s != 0)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 	return (0);
 }
 
