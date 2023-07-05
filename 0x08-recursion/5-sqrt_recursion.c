9d33fe22b5701bd610a0db0fba5eea0a9a4bc8d9
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-06 02:50:43 +0300

    modified:   5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,27 +8,14 @@
  * If n does not have a natural square root, the function should return -1
  */
 
-long sr(long n, long x)
+int sr(int n, int x)
 {
-	if (x <= 1)
+	if (x * x == n)
+		return (x);
+	if (x * x < n)
+		return (sr(n, x + 1));
+	else /** (x * x > n)*/
 		return (-1);
-	if (n % x == 0)
-	{
-		if (x * x == n)
-			return (x);
-		if (x * x > n)
-			return (sr(n, x - 1));
-		if (x * x < n)
-			return (-1);
-	}
-	if (n % x != 0)
-	{
-		if (x * x > n)
-			return (sr(n, x - 1));
-		if (x * x < n)
-			return (-1);
-	}
-	return (-1);
 }
 
 /**
@@ -46,5 +33,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return ((int)sr((long)n, (long)(n / 2)));
+	return (sr(n, 2));
 }
