768e924de3db20b9de0d26391c868574abc964f6
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-08 05:49:16 +0300

    modified:   create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c && ar -rc liball.a *.c && ar -t liball.a
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c && ar -rc liball.a *.o && ar -t liball.a
