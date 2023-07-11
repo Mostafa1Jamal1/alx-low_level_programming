fbf23739c889b804659d0a4b9f97a001049f2816
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-11 21:50:51 +0300

    Task3 in 0x0B.C-malloc, free project and modified README.md and main.h

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a 2 dimensional array of integers.
+ *
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: a pointer to the 2 2 dimensional array.
+ * or NULL on failure or width or height is 0 or negative.
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (0);
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == 0)
+		return (0);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == 0)
+			return (0);
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
