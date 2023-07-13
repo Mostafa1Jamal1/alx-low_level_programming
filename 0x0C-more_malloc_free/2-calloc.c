14ff6f403f8e13d4a628cf37021093d73029303f
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-14 02:10:35 +0300

    modified:   2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == 0)
 		return (0);
 	cptr = ptr;
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		*(cptr + i) = 0;
 	return (ptr);
 }
