00d522f8f37162a381254adb76aafe3a25963503
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-13 21:03:24 +0300

    modified:   2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i;
-    char *ptr;
+	unsigned int i;
+	char *ptr;
 
-    if (size == 0)  /* check if size = 0 and return 0 when true*/
-        return (0);
-    ptr = malloc(nmemb * size); /* Allocating */
-    if (ptr == 0)
-        return (0);
-    for (i = 0; i < nmemb; i++)
-        *(ptr + i) = 0;
-    return (ptr);
+	if (size == 0)  /* check if size = 0 and return 0 when true*/
+		return (0);
+	ptr = malloc(nmemb * size); /* Allocating */
+	if (ptr == 0)
+		return (0);
+	for (i = 0; i < nmemb; i++)
+		*(ptr + i) = 0;
+	return (ptr);
 }
