8e1b55672b3ff9f85a4217929c4b7c0a77de3cca
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-13 19:41:22 +0300

    Task1 in 0x0C.C-More malloc, free project and modified README.md and main.h

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+/**
+ * _strlen - calculate the length of a string
+ * @s: the string.
+ * Return: the number of bytes of the string without the terminating NULL
+*/
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+	return (i);
+}
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: is a pointer to the first string.
+ * @s2: is a pointer to the second string.
+ * @n: the number of bytes that will be added from s2 to s1
+ *
+ * Return: a pointer to newly allocated space in memory
+ * which contains s1, followed by the first n bytes of s2, and null terminated
+ * on failing return NULL.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1len, s2len, i;
+	char *str;
+
+	s1len = _strlen(s1);    /*get the length of s1*/
+	s2len = _strlen(s2);    /*get the length of s2*/
+	if (n >= s2len)
+	{   /* Allocating only the necessary space */
+		str = malloc(s1len + s2len + 1);
+		if (str == 0)
+			return (0);
+		/* copying s1 to str */
+		for (i = 0; *(s1 + i) != 0; i++)
+			*(str + i) = *(s1 + i);
+		/* copying s2 to str */
+		for (i = 0; *(s2 + i) != 0; i++)
+			*(str + i + s1len) = *(s2 + i);
+		/* Adding a terminating NULL */
+		*(str + s1len + s2len) = 0;
+	}
+	else
+	{
+		/* Allocating only the necessary space */
+		str = malloc(s1len + n + 1);
+		if (str == 0)
+			return (0);
+		/* copying s1 to str */
+		for (i = 0; *(s1 + i) != 0; i++)
+			*(str + i) = *(s1 + i);
+		/* copying n chars from s2 to str */
+		for (i = 0; i < n; i++)
+			*(str + i + s1len) = *(s2 + i);
+		/* Adding a terminating NULL */
+		*(str + s1len + n) = 0;
+	}
+	return (str);
+}
