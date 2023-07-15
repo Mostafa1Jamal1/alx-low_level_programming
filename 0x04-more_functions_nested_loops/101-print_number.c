3eb7f42c8a72547ebcd6a14f9b3463119066a806
Mostafa_home <mostafa.jamal.mjm@gmail.com>
2023-07-15 21:31:15 +0300

    modified:   101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@
 
 void print_number(int n)
 {
-	unsigned int rev = 0;
+	unsigned int dev = 1;
 	/* when n = 0 not necessary to complete */
 	if (n == 0)
 	{
@@ -23,20 +23,23 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
+		/* Check for int-min before convert to +*/
+		if (n < -2147483647)
+		{
+			_putchar(-(n / 1000000000) + '0');
+			n %= 1000000000;
+		}
+		/* Convert to positive */
 		n *= -1;
 	}
-	/* Revrese the number */
-	while (n != 0)
+	/* Count how many digits */
+	while (n / dev >= 10)
+		dev *= 10;
+	/* Print from biggest place */
+	while (dev != 0)
 	{
-		rev += n % 10;
-		if ((n / 10) != 0)
-			rev *= 10;
-		n /= 10;
-	}
-	/* Print in reverse */
-	while (rev != 0)
-	{
-		_putchar((rev % 10) + '0');
-		rev /= 10;
+		_putchar((n / dev) + '0');
+		n %= dev;
+		dev /= 10;
 	}
 }
