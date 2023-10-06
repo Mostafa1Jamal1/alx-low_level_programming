7301fd2e8027105fdfc550e195c9bc9adf208549
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-07 02:48:21 +0300

    Check for ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -65,9 +65,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	size_t val_len;
 
 	/* key can not be an empty string */
-	if (key == NULL || value == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
-	if (strlen(key) == 0 || *key == '\0')
+	if (strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
