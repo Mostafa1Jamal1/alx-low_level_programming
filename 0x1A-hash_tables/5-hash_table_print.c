e4e1188846f29b6da66e877c81db0c8189da3eb4
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-07 02:53:04 +0300

    Task5 in 0x1A.C-Hash tables project

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht:  is the hash table
+ * should print the key/value in the order that
+ * they appear in the array of hash table
+ * Order: array, list
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int separate = 0;
+
+	printf("{");
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			node = ht->array[i];
+			if (node == NULL)
+				continue;
+			else
+			{
+				if (separate != 0)
+					printf(", ");
+				separate = 1;
+				printf("'%s': '%s'", node->key, node->value);
+			}
+		}
+	}
+	printf("}\n");
+}
