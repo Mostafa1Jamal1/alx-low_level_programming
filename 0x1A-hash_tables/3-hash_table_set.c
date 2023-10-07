d524cf4f79f62025f1704c2cd6737082b930acb4
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-07 04:50:20 +0300

    style: betty fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -69,14 +69,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
-	
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{ /* Case if the key exists update the value */
 			free(current->value);
 			current->value = strdup(value);
-			return(1);
+			return (1);
 		}
 		current = current->next;
 	}
